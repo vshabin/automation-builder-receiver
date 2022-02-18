276d76ff190af3649c9a826024d547bb522d0b24
PavelVydysh <vydysh.pavel@gmail.com>
2022-02-18 19:43:13 +0200

    Receiver(нужен рефакторинг)

---
+++ src/main/java/com/reciever/Reciever.java
@@ -0,0 +1,27 @@
+package com.reciever;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+public class Reciever {
+
+    private final static String QUEUE_NAME = "reciever_message";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println(" [x] Received '" + message + "'");
+        };
+        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
+    }
+
+}
